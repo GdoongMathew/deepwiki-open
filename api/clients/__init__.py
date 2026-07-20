b3361426cc393e969ae434704501002f50f92703
GdoongMathew <gdoongmathew@gmail.com>
2026-07-20 13:45:48 +0800

    import documentations

--- api/clients/__init__.py
+++ api/clients/__init__.py
@@ -1,10 +1,18 @@
+"""Unify Adalflow compatible clients to here.
+Any patches and additional clients could be applied or imported in this module.
+"""
+
 from .google_embedder import GoogleEmbedderClient
 from .bedrock import BedrockClient
 from .dashscope import DashscopeClient
 from .ollama import OllamaClient
 from .litellm import LiteLLMClient
 from .openrouter import OpenRouterClient
-from adalflow.components.model_client import AzureAIClient, OpenAIClient, GoogleGenAIClient
+from adalflow.components.model_client import (
+    AzureAIClient,
+    OpenAIClient,
+    GoogleGenAIClient,
+)
 
 __all__ = [
     "AzureAIClient",
